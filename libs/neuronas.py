c34b7fe693eee1e08e60bde4758f19dccd483077
balde <cbaldelomar@hotmail.com>
2025-07-26 15:20:04 -0600

    Preparar la neurona para predecir sobre lote de datos

--- libs/neuronas.py
+++ libs/neuronas.py
@@ -6,12 +6,13 @@ class NeuronaPerceptron:
         self.entradas = entradas
         self.salidas = salidas
         self.pesos_Ws = None
-        self.sesgos_bias = None
-        self.__iniciar_pesos_y_bias()
+        self.bias = None
 
     def predecir_feedforward(self, muestra_datos):
         if self.__is_one_dimension(muestra_datos):
             return self.__activacion(self.__sumatoria(muestra_datos))
+
+        return self.__activacion_lote(self.__sumatoria_lote(muestra_datos))
 
     def __is_one_dimension(self, muestra_datos):
         return isinstance(muestra_datos, np.ndarray) and muestra_datos.ndim == 1
@@ -39,8 +40,28 @@ class NeuronaPerceptron:
         # z_sumatoria_muestra_datos = z_sumatoria_muestra_datos + self.bias
         # return z_sumatoria_muestra_datos
 
+    def __sumatoria_lote(self, muestra_datos):
+        return [self.__sumatoria(subarray) for subarray in muestra_datos]
+
+        # mis_Zs = []
+        # # Hay que iterar sobre todas las filas, y fila por fila.
+        # for i in range(len(muestra_datos[:, 0])):
+        #     # Posteriormente, ubicado en cada fila, mandar a llamar a "__sumatoria".
+        #     muestra_datos_i = muestra_datos[i, :]
+        #     z_sumatoria_muestra_datos = self.__sumatoria(muestra_datos_i)
+        #     mis_Zs.append(z_sumatoria_muestra_datos)
+        # return mis_Zs
+
     def __activacion(self, Z):
         if Z >= 0:
             return 1
 
         return 0
+
+    def __activacion_lote(self, Zs):
+        return [self.__activacion(z) for z in Zs]
+
+        # mis_As = []
+        # for z in Zs:
+        #     mis_As.append(self.__activacion(z))
+        # return mis_As
